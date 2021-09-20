c3cf34faecd3feee7caf5cb821ad1b6eb14a2a2b
kaseken <k.kaseda0925@gmail.com>
2021-09-20 16:06:24 +0900

    feat: chapter 10 to 12 (#4)

--- src/main/scala/money/Franc.scala
+++
@@ -1,5 +0,0 @@
-package money
-
-class Franc(amount: Int, currency: String) extends Money(amount, currency) {
-  def *(multiplier: Int): Money = Money.franc(amount * multiplier)
-}
--- src/test/scala/money/MoneySuite.scala
+++ src/test/scala/money/MoneySuite.scala
@@ -3,7 +3,7 @@ package money
 import org.scalatest.funsuite.AnyFunSuite
 
 class MoneySuite extends AnyFunSuite {
-  test("test Dollar multiplication") {
+  test("multiplication") {
     val five = Money.dollar(5)
     assert(five * 2 == Money.dollar(10))
     assert(five * 3 == Money.dollar(15))
@@ -12,19 +12,19 @@ class MoneySuite extends AnyFunSuite {
   test("equality") {
     assert(Money.dollar(5) == Money.dollar(5))
     assert(!(Money.dollar(5) == Money.dollar(6)))
-    assert(Money.franc(5) == Money.franc(5))
-    assert(!(Money.franc(5) == Money.franc(6)))
     assert(!(Money.dollar(5) == Money.franc(5)))
-  }
-
-  test("test Franc multiplication") {
-    val five = Money.franc(5)
-    assert(five * 2 == Money.franc(10))
-    assert(five * 3 == Money.franc(15))
   }
 
   test("currency") {
     assert(Money.dollar(1).currency == "USD")
     assert(Money.franc(1).currency == "CHF")
+  }
+
+  test("simple addition") {
+    val five = Money.dollar(5)
+    val sum = five + five
+    val bank = new Bank()
+    val reduced = bank.reduce(sum, "USD")
+    assert(reduced == Money.dollar(10))
   }
 }
