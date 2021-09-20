c8984cb4ce693facdc2478a34e010a390132edb8
kaseken <k.kaseda0925@gmail.com>
2021-09-20 17:55:14 +0900

    feat: chapter 16 to 17

--- src/main/scala/money/Bank.scala
+++ src/main/scala/money/Bank.scala
@@ -8,9 +8,7 @@ class Bank {
   private val rates = new mutable.HashMap[Pair, Int]()
   def reduce(source: Expression, to: String): Money = source.reduce(this, to)
 
-  def addRate(from: String, to: String, rate: Int): Option[Int] = {
-    rates.put(Pair(from, to), rate)
-  }
+  def addRate(from: String, to: String, rate: Int): Option[Int] = rates.put(Pair(from, to), rate)
 
   def rate(from: String, to: String): Int = {
     if (from == to) 1 else rates.getOrElse(Pair(from, to), 1) // FIXME
