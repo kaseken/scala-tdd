77501b472f320784c4c4af1cb247dfe233844f0b
kaseken <k.kaseda0925@gmail.com>
2021-09-20 17:35:00 +0900

    feat: chapter 13 to 15 (#5)

--- src/test/scala/money/MoneySuite.scala
+++ src/test/scala/money/MoneySuite.scala
@@ -27,4 +27,44 @@ class MoneySuite extends AnyFunSuite {
     val reduced = bank.reduce(sum, "USD")
     assert(reduced == Money.dollar(10))
   }
+
+  test("test + returns sum") {
+    val five = Money.dollar(5)
+    val sum = five + five
+    assert(five == sum.augend)
+    assert(five == sum.addend)
+  }
+
+  test("reduce money") {
+    val bank = new Bank()
+    val result = bank.reduce(Money.dollar(1), "USD")
+    assert(result == Money.dollar(1))
+  }
+
+  test("reduce sum") {
+    val sum = new Sum(Money.dollar(3), Money.dollar(4))
+    val bank = new Bank()
+    val result = bank.reduce(sum, "USD")
+    assert(result == Money.dollar(7))
+  }
+
+  test("reduce money different currency") {
+    val bank = new Bank()
+    bank.addRate("CHF", "USD", 2)
+    val result = bank.reduce(Money.franc(2), "USD")
+    assert(result == Money.dollar(1) )
+  }
+
+  test("identical rate") {
+    assert(1 == new Bank().rate("USD", "USD"))
+  }
+
+  test("mixed addition") {
+    val fiveBucks = Money.dollar(5)
+    val tenFrancs = Money.franc(10)
+    val bank = new Bank
+    bank.addRate("CHF", "USD", 2)
+    val result = bank.reduce(fiveBucks + tenFrancs, "USD")
+    assert(result == Money.dollar(10))
+  }
 }
